4ced32105ea1938cfe1611e62c63ecfa76975114
rimonspi12 <rimonmpi@gmail.com>
2021-10-18 11:29:44 +0600

    carousel added

--- src/component/Home/Header/Header.js
+++ src/component/Home/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Carousel, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../images/logo3.jpg';
 import './Header.css';
+import pic1 from '../../../images/carousel/1.jpg';
+import pic2 from '../../../images/carousel/2.jpg';
+import pic3 from '../../../images/carousel/5.jpg';
+
+
+
+
 const Header = () => {
     return (
         <div>
+            {/* navbar start */}
             <Navbar bg="light" expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img
+                        <img id="img-style"
                             alt="not show"
                             src={logo}
                             width="80"
@@ -29,6 +37,55 @@ const Header = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            {/* navbar ends */}
+
+            {/* carousel start */}
+
+
+
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 img-size"
+                        src={pic1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="fw-bold">See our Medical Team responsibility</h3>
+                        <p className="fw-bold">We are 100 Medical expert team they are always service patience and take care properly</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={pic2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Our Mother Care Doctor</h3>
+                        <p>Our Mother care doctor always support our pregnant mother all the situations</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={pic3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Our Special Doctor Team</h3>
+                        <p>They are always handle any serious moment patient.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+
+
+
+
+            {/* carousel ends */}
         </div>
     );
 };
