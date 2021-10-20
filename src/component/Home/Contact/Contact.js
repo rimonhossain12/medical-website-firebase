92349ceffe08421cbecd57b1f4330b9f4f49ab06
rimonspi12 <rimonmpi@gmail.com>
2021-10-20 19:40:57 +0600

    location is added

--- src/component/Home/Contact/Contact.js
+++ src/component/Home/Contact/Contact.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import contImg from '../../../images/Clients/contact.jpg';
+import { Col, Row } from 'react-bootstrap';
 
 const Contact = () => {
     return (
         <div>
            <Header></Header>
-           <div>
-               <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates, itaque architecto! Maxime soluta neque magnam quod animi provident nobis repellendus assumenda. Temporibus, suscipit natus rerum magnam praesentium laborum magni maxime ipsam consequatur commodi esse amet est veniam necessitatibus facere nisi aspernatur accusamus atque fugiat impedit ex deserunt et quasi. Blanditiis.</h2>
+            <h5 className="mt-5 text-info text-bold">Our location details</h5>
+            <hr className="w-25 mx-auto"/>
+            <div className="d-flex container mt-5">
+                <Row xs={1} md={2} xl={3} className="g-2">
+                    <Col>
+                    <img src={contImg}  alt="" />
+                    </Col>
+                    <Col className="ms-5">
+                        <h4 className="text-center text-info">Where are our location?</h4>
+                        <p className="text-start text-primary">Dhaka Banani 102, Dhaka Sheikh Kamal Hospital</p>
+                      <p className="text-start text-primary">Phone: +8883012831038</p>
+
+                        <p className="text-start text-primary">Email: rimonmpi@gmail.com</p>
+                        <p className="text-start text-primary">Facebook: sheikhKamal.medical.hospital-230</p>
+
+                    </Col>
+                </Row>
            </div>
            <Footer></Footer>
         </div>
